0c835b275dfa53255c20b8e0f1e52d76e0ad44a5
Esther <esther@RYANs-MacBook-Pro.local>
2022-09-22 08:45:12 -0600

    Model Created

---
+++ TravelDiary/Model/Log.swift
@@ -0,0 +1,26 @@
+//
+//  Log.swift
+//  TravelDiary
+//
+//  Created by Esther on 9/22/22.
+//
+
+import Foundation
+
+// Title, Address, Date, Body
+
+class Log {
+    
+    // MARK: - Properties
+    
+    let title: String
+    let address: String
+    let logDate: Date
+    let body: String
+    
+    // MARK: - Initializer
+    init(title: String, address: String, logDate: Date, body: String){
+        
+    }
+
+}
