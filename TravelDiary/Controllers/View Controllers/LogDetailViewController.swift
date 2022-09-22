92be0e8d7eaca0f12e42022ce5f5f7a2a5630809
Esther <esther@RYANs-MacBook-Pro.local>
2022-09-22 10:52:01 -0600

    all wired up

--- TravelDiary/Controllers/View Controllers/LogDetailViewController.swift
+++ TravelDiary/Controllers/View Controllers/LogDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  LogViewController.swift
+//  LogDetailViewController.swift
 //  TravelDiary
 //
 //  Created by Esther on 9/22/22.
@@ -7,8 +7,17 @@
 
 import UIKit
 
-class LogViewController: UIViewController {
+class LogDetailViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet weak var logDateLabel: UILabel!
+    @IBOutlet weak var logTitleLable: UITextField!
+    @IBOutlet weak var logAddressLabel: UITextField!
+    @IBOutlet weak var logTextView: UITextView!
+    @IBOutlet weak var logTitleTextField: UITextField!
+    @IBOutlet weak var logAddressTextField: UITextField!
+    @IBOutlet weak var logBodyTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
